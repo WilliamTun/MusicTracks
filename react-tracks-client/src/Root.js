ebff48e198bb1e300cacc2bc78df7e78fb8217f5
WilliamTun <willtun1991@gmail.com>
2021-05-07 14:19:27 +0100

    header

--- react-tracks-client/src/Root.js
+++ react-tracks-client/src/Root.js
@@ -1,29 +1,56 @@
 import React from "react";
-import withRoot from "./withRoot";
 import { Query } from 'react-apollo'
 import {gql} from 'apollo-boost'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import withRoot from "./withRoot";
+import App from './pages/App';
+import Profile from './pages/Profile';
+import Header from './components/Shared/Header'
 
 const Root = () => (
-    <Query query={GET_TRACKS_QUERY}>
+    <Query query={ME_QUERY}>
         { ({ data, loading, error }) => {
                 if (loading) return <div> Loading </div>
                 if (error) return <div> Error </div>
 
-                return <div> {JSON.stringify(data)} </div>
+                return (
+                    <Router>  
+                        <>
+                        <Header />
+                        <Switch>
+                            <Route exact path="/" component={App}/>
+                            <Route path="/profile/:id" component={Profile}/>
+                        </Switch>
+                        </>
+                    </Router>
+                ) 
+                
+                //<div> {JSON.stringify(data)} </div>
             }
         }
     </Query>
 )
 
-const GET_TRACKS_QUERY = gql`
+const ME_QUERY = gql`
     {
-        tracks {
+        me {
             id 
-            title
-            description
-            url
+            username 
+            email 
         }
     }
 `
+
+//const GET_TRACKS_QUERY = gql`
+//    {
+//        tracks {
+//            id 
+//            title
+//            description
+//            url
+//        }
+//    }
+//`
 
 export default withRoot(Root);
